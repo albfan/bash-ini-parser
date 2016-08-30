f589bc795da4e7a045f819a4f7031629a93beddb
albfan <albertofanjul@gmail.com>
2016-08-30 14:51:29 +0200

    Add continuous integration

--- t/t0004-comments.sh
+++ t/t0004-comments.sh
@@ -9,8 +9,8 @@ DIR_TEST=$SHARNESS_TEST_DIRECTORY/t0004
 test_expect_success "Parse comments" "
     export COVERAGE_NAME=comments_parser
     cp ../.simplecov .
-    cfg_parser $DIR_TEST/comments.ini
-    cfg_writer > comments.out
+    "$COMMAND" cfg_parser $DIR_TEST/comments.ini
+    "$COMMAND" cfg_writer > comments.out
     diff $DIR_TEST/comments.out.correct comments.out
 "
 
