0ce2bee87b683484e6d7f2fb3d5e8e8416bd38f2
Pavel Rehak <pavel-rehak@email.cz>
2018-05-25 19:47:53 +0200

    Update variable test.

---
+++ t/t0006-duplication.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+test_description="check duplication"
+
+. setup.sh
+
+DIR_TEST=$SHARNESS_TEST_DIRECTORY/t0006
+
+test_expect_failure "Remove duplication" "
+    export COVERAGE_NAME=duplication_parser
+    cp ../.simplecov .
+    cfg_parser $DIR_TEST/duplication.ini
+    export COVERAGE_NAME=duplication_section
+    cfg_section_sec1
+    var1=bar
+    export COVERAGE_NAME=duplication_update
+    cfg_update sec1 var1
+    export COVERAGE_NAME=duplication_writer
+    cfg_writer > duplication.out
+    diff $DIR_TEST/duplication.out.correct duplication.out
+"
+
+test_done
