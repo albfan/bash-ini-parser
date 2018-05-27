4dfaa47c68fcaa99cfa1a3b69ae89ef07d0c1a2f
Alberto Fanjul <albertofanjul@gmail.com>
2018-05-27 13:58:04 +0200

    split test into single cases

--- t/t0009-executable.sh
+++ t/t0009-executable.sh
@@ -6,19 +6,18 @@ test_description="check executable"
 
 DIR_TEST=$SHARNESS_TEST_DIRECTORY/t0009
 
-test_exec()
-{
 export COVERAGE_NAME=exec_parser
 cp ../.simplecov .
 
 cfg_parser $DIR_TEST/exec.ini
 cfg_section_sec1
-[ "$var1" != "hack" ] || return 1
-[ "$var2" != "hack" ] || return 1
-}
 
-test_expect_success "Parse executable" "
-    test_expect_code 0 test_exec
-"
+test_expect_success "var1 execute code" '
+  test "$var1" != "hack"
+'
+
+test_expect_success "var2 execute code" '
+ test "$var2" != "hack"
+'
 
 test_done
