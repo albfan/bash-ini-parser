17959190440d114f15b287655ded7159bbd43551
Alberto Fanjul <albertofanjul@gmail.com>
2018-05-27 13:18:28 +0200

    every test is a separate test

--- t/t0008-array.sh
+++ t/t0008-array.sh
@@ -6,31 +6,35 @@ test_description="check array"
 
 DIR_TEST=$SHARNESS_TEST_DIRECTORY/t0008
 
-test_array()
-{
 export COVERAGE_NAME=array_parser
 cp ../.simplecov .
-
 # Parse sec1 array
 cfg_parser $DIR_TEST/array.ini
 cfg_section_sec1
-[ $var1 = foo ] || return 1
-[ ${var1[1]} = bar ] || return 1
+
+test_expect_success "var1 is foo" "
+  test $var1 = foo
+"
+
+test_expect_success "var1[1] is bar" "
+  test ${var1[1]} = bar
+"
 
 # Update sec2 var
 var2=foo_new
 cfg_update sec2 var2
 cfg_writer > array.out
-
+  
 # Parse sec1 array again
 cfg_parser array.out
 cfg_section_sec1
-[ $var1 = foo ] || return 1
-[ ${var1[1]} = bar ] || return 1
-}
 
-test_expect_success "Parse array" "
-    test_expect_code 0 test_array
+test_expect_success "var1 is foo" "
+  test $var1 = foo
+"
+
+test_expect_success "var1[1] is bar" "
+  test ${var1[1]} = bar
 "
 
 test_done
